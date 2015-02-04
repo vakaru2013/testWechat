688bd482a54341191f37c651a0928f6156544814
vakaru2013 <18871392@qq.com>
2015-02-05 00:11:56 +0800

    换行的问题不管了先。这次，如果query string中有echostr，就返回它的值，否则返回welcome字符串

--- index.py
+++ index.py
@@ -1,17 +1,21 @@
 #-*- coding:utf-8 -*-
 
+from cgi import parse_qs, escape
+
 def app(environ, start_response):
     status = '200 OK'
     headers = [('Content-type', 'text/html')]
     start_response(status, headers)
-    body=["Welcome to Baidu Cloud!\n"]
     
-    # 遍历environ字典
-    inputs=["%s==%s" % (k,x) for (k,x) in environ.items()]
+    # 取出query string的值
+    # Returns a dictionary containing lists as values.
+    d = parse_qs(environ['QUERY_STRING'])
+    echostr = d.get('echostr', []) # Returns a list of echostr.
+    if(len(echostr)>0):
+        return [echostr[0]]
     
-    # 将environ中所有的键值对输出
-    body+=inputs
-    body='\r\n\r\n'.join(body)
+    # 如果query string中没有echostr就返回下面的字符串
+    body=["Welcome to Baidu Cloud!\n This is test.py!\n"]
     return [body]
 
 from bae.core.wsgi import WSGIApplication
