d0f2419a4c242a70b09436ac0465d93bdf06ba4a
vakaru2013 <18871392@qq.com>
2015-03-06 18:37:07 +0800

    正在实现一个微信交友功能，使用redis数据库，coding中 ------ 内部都用unicode字符串，与外部的交互才考虑用utf-8

--- sns1/commondef.py
+++ sns1/commondef.py
@@ -8,44 +8,50 @@ from baeredis import redisConn
 # 这个模块定义了一些公用函数、字符串常量
 
 # 下面是根据用户的id组装'在redis数据库存储用户状态的key'的函数
+# 输出是unicode
 def makeUserStateKey(userid):
-    return 'userState-%s' % userid
+    return u'userState-%s' % userid
     
 # 下面是根据用户的id组装'在redis数据库存储用户自我介绍的key'的函数
+# 输出是unicode
 def makeUserIntroKey(userid):
-    return 'userIntro-%s' % userid
+    return u'userIntro-%s' % userid
 
 # 下面是用户所处的状态的值，是一些字符串常量：
-initState='init'
-toRecvIntroState='to_recv_intro'
-toRecvCmdState='to_recv_cmd'
+initState=u'init'
+toRecvIntroState=u'to_recv_intro'
+toRecvCmdState=u'to_recv_cmd'
 
 # 下面是回复给微信客户端的text文本，是一些字符串常量
 msgWaitForIntro=u'你还没有填写自我介绍，请发送你的自我介绍。'
 msgWaitForCmd=u'你当前的自我介绍是：%s。想修改自我介绍请发送1，想查看在线用户资料请发送2，想检查你的收件箱请发送3。'
 msgError=u'遇到了未知的错误'
 
-# 这个函数从redis数据库中读取用户的状态
+# 这个函数从redis数据库中读取用户的状态，
+# 返回的是unicode字符串
 def getUserState(userid):
     key=makeUserStateKey(userid)
-    state=redisConn.get(key)
+    state=redisConn.get(key.encode('utf-8'))
     if state==None:
         state=initState
-    return state
+    return state.decode('utf-8')
     
 # 这个函数在redis数据库中设置用户的状态
+# 输入都是unicode字符串
 def setUserState(userid,state):
     key=makeUserStateKey(userid)
-    redisConn.set(key,state)
+    redisConn.set(key.encode('utf-8'),state.encode('utf-8'))
     
 # 这个函数从redis数据库中读取用户的自我介绍，如果没有自我介绍，则返回None
+# 输入输出都是unicode字符串
 def getUserIntro(userid):
     key=makeUserIntroKey(userid)
-    intro=redisConn.get(key)
-    return intro
+    intro=redisConn.get(key.encode('uft-8'))
+    return intro.decode('utf-8')
     
 # 这个函数在redis数据库中设置用户的自我介绍
+# 输入都是unicode字符串
 def setUserIntro(userid,intro):
     key=makeUserIntroKey(userid)
-    redisConn.set(key,intro)
+    redisConn.set(key.encode('uft-8'),intro.encode('uft-8'))
     
